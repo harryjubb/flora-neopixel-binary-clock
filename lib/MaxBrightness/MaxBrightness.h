678c2db4135d66029398b400a99b7d58f084cf4e
Harry Jubb <harry.jubb@gmail.com>
2020-09-21 00:13:53 +0100

    Max brightness setup and descriptive comments

---
+++ lib/MaxBrightness/MaxBrightness.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 harry
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// DO NOT EDIT: Must be 50 at all times
+#define MAX_BRIGHTNESS 50
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,59 @@
 // Copyright (c) 2020 harry
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <MaxBrightness.h>
 
+// Pin 6 on the Flora is a PWM pin suitable for the NeoPixel data line
 #define STRIP_PIN 6
+
+/*
+Always expecting a strip of 20 NeoPixels for a binary-coded decimal clock
+Using a different strip length may need a different consideration for MAX_BRIGHTNESS
+
+    8    8    8
+    4  4 4  4 4
+  2 2  2 2  2 2
+  1 1  1 1  1 1
+
+  H H  M M  S S
+
+*/
 #define STRIP_LENGTH 20
 
-// Do *NOT*
+/*
+Do *NOT* set brightness > MAX_BRIGHTNESS (which should be 50 for a "default" 20 NeoPixel setup)
+eerkmans.nl/powering-lots-of-leds-from-arduino/
+*/
 #define BRIGHTNESS 20
 
 // Strip of NeoPixels for the binary clock
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIP_LENGTH, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
-  // Setup code here, to run once
+// Setup code here, to run once
+void setup()
+{
+  // Failsafe against setting brightness too high:
+  // Never allow setting up NeoPixels with too high a brightness
+  if (BRIGHTNESS > MAX_BRIGHTNESS)
+  {
+    return;
+  }
+
   strip.begin();
   strip.setBrightness(BRIGHTNESS);
   strip.show(); // Initialize all pixels to 'off' (clearing any previous state)
 }
 
-void loop() {
-  // Main code here, to run repeatedly
+// Main code here, to run repeatedly
+void loop()
+{
+  // Failsafe against setting brightness too high
+  if (BRIGHTNESS > MAX_BRIGHTNESS)
+  {
+    return;
+  }
 }
