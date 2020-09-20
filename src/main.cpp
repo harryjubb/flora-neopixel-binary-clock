34c440d9e048295c4e29de3e36a79da4c2f21ebf
Harry Jubb <harry@harrys-mbp-3.home>
2020-09-20 15:28:41 +0100

    Strip setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 
 #define STRIP_PIN 6
 #define STRIP_LENGTH 20
+#define BRIGHTNESS 20
 
+// Strip of NeoPixels for the binary clock
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIP_LENGTH, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-
 void setup() {
-  // put your setup code here, to run once:
+  // Setup code here, to run once
+  strip.begin();
+  strip.setBrightness(BRIGHTNESS);
+  strip.show(); // Initialize all pixels to 'off' (clearing any previous state)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Main code here, to run repeatedly
 }
